804414328df850c15230bae8fc32f170e7567ef8
Leonel Cordova <leonelcordova.bi@gmail.com>
2020-07-09 14:11:26 -0400

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { WebsocketService } from './services/websocket.service';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client-ang';
+
+  title = 'client';
+
+  constructor(
+    public wsService: WebsocketService
+  ) {}
+
 }
